ddcb7ca013b71561a731e9eaf59f957e41bfc3e9
Студент группы IS <is@sielom.local>
2025-09-26 09:44:53 +0500

    project1.2

--- hello_app/models.py
+++ hello_app/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 
+# Create your models here.
+
+
 class Author(models.Model):
     name = models.CharField(verbose_name='Имя автора', max_length=20)
-    surname = models.CharField("Фамилия", max_length=25)
-    birthday = models.DateField("Дата рождения")
-    bio = models.TextField("Биография")
+    surname = models.CharField("Фамилия",max_length=25)
+    bitrhday = models.DateField("Биография", default="1999-02-02")
+    bio = models.TextField("Описание")
+    desc = models.CharField("Жив или мертв", default="No")
+
+    class Meta: 
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['surname','name']
+        indexes = [
+            models.Index(fields=['surname'])
+        ]
+        constraints = [
+            models.UniqueConstraint (
+                fields = ['surname','bio'],
+                condition=models.Q(desc='Жив'),
+                name = 'unique_surname_bio'
+            ),
+        ]
 
 class Publisher(models.Model):
-    name = models.CharField("Название", unique=True, )
+    name = models.CharField("Название",unique=True)
 
 class Book(models.Model):
-    title = models.CharField("Название", max_length=50)
+    title = models.CharField("Названиие",max_length=50)
     id_publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
-    id_author = models.ManyToManyField(Author)
