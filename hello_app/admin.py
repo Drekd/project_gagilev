9460738f66a2458c6498c808621c5aabad5434f1
Студент группы IS <is@sielom.local>
2025-10-01 12:22:24 +0500

    projecr1.5

--- hello_app/admin.py
+++ hello_app/admin.py
@@ -3,6 +3,9 @@ from.models import *
 
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Publisher)
+admin.site.register(Post)
+admin.site.register(Seller)
+admin.site.register(Categories)
+admin.site.register(Car)
+admin.site.register(Order)
+admin.site.register(Costumer)
